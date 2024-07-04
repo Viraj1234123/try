d784b39c98fe3a4109be86f28b36ce7d3658ed25
Viraj <vis68354@gmail.com>
2024-07-04 13:54:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,66 +1,240 @@
-require('dotenv').config()
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User.js');
+const Place = require('./models/Place.js');
+const Booking = require('./models/Booking.js');
+const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+const fs = require('fs');
+const mime = require('mime-types');
 
-const app = express()
+require('dotenv').config();
+const app = express();
 
-const port = 4000
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgwiuytfd';
+const bucket = 'dawid-booking-app';
 
-const githubData = {
-    "login": "hiteshchoudhary",
-    "id": 11613311,
-    "node_id": "MDQ6VXNlcjExNjEzMzEx",
-    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/hiteshchoudhary",
-    "html_url": "https://github.com/hiteshchoudhary",
-    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
-    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
-    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
-    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
-    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
-    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
-    "type": "User",
-    "site_admin": false,
-    "name": "Hitesh Choudhary",
-    "company": null,
-    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
-    "location": "India",
-    "email": null,
-    "hireable": null,
-    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
-    "twitter_username": "hiteshdotcom",
-    "public_repos": 71,
-    "public_gists": 1,
-    "followers": 7037,
-    "following": 0,
-    "created_at": "2015-03-23T13:03:25Z",
-    "updated_at": "2023-09-09T03:27:44Z"
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
+app.use(cors({
+  credentials: true,
+  
+  origin: process.env.ORIGIN,
+}));
+
+function getUserDataFromReq(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
+}
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        //fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        //fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+app.get('/api/test', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  res.json('test ok');
+});
+
+app.post('/api/register', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {name,email,password} = req.body;
+
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password:bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+
+});
+
+app.post('/api/login', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {email,password} = req.body;
+  const userDoc=await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id
+      }, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token, {
+          sameSite: 'none',
+          secure: true,
+        }).json(userDoc);
+      });
+    } else {
+      res.status(422).json('pass not ok');
+    }
+  } else {
+    res.status(422).json('not found');
+  }
+});
+
+app.get('/api/profile', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post('/api/logout', (req,res) => {
+  res.cookie('token', '').json(true);
+});
+
+
+app.post('/api/upload-by-link', async (req,res) => {
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  try {
+    await imageDownloader.image({
+      url: link,
+      dest: '/tmp/' +newName,
+    });
+    const url = await uploadOnCloudinary('/tmp/' +newName);
+    res.json(url.url);
+  } catch (error) {
+    res.status(200).send('Error');
   }
   
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const photosMiddleware = multer({dest:'/tmp'});
+app.post('/api/upload', photosMiddleware.array('photos', 100), async (req,res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const {path} = req.files[i];
+    const url = await uploadOnCloudinary(path);
+    uploadedFiles.push(url.secure_url);
+  }
+  res.json(uploadedFiles);
+});
 
-app.get('/twitter', (req, res) => {
-    res.send('hiteshdotcom')
-})
+app.post('/api/places', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  const {
+    title,address,addedPhotos,description,price,
+    perks,extraInfo,checkIn,checkOut,maxGuests,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner:userData.id,price,
+      title,address,photos:addedPhotos,description,
+      perks,extraInfo,checkIn,checkOut,maxGuests,
+    });
+    res.json(placeDoc);
+  });
+});
 
-app.get('/login', (req, res) => {
-    res.send('<h1>please login at chai aur code</h1>')
-})
+app.get('/api/user-places', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const {id} = userData;
+    res.json( await Place.find({owner:id}) );
+  });
+});
 
-app.get('/youtube', (req, res) => {
-    res.send("<h2>Chai aur code</h2>")
-})
+app.get('/api/places/:id', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {id} = req.params;
+  res.json(await Place.findById(id));
+});
 
-app.get('/github', (req, res) => {
-    res.json(githubData)
-})
+app.put('/api/places', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  const {
+    id, title,address,addedPhotos,description,
+    perks,extraInfo,checkIn,checkOut,maxGuests,price,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title,address,photos:addedPhotos,description,
+        perks,extraInfo,checkIn,checkOut,maxGuests,price,
+      });
+      await placeDoc.save();
+      res.json('ok');
+    }
+  });
+});
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.get('/api/places', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  res.json( await Place.find() );
+});
+
+app.post('/api/bookings', async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const userData = await getUserDataFromReq(req);
+  const {
+    place,checkIn,checkOut,numberOfGuests,name,phone,price,
+  } = req.body;
+  Booking.create({
+    place,checkIn,checkOut,numberOfGuests,name,phone,price,
+    user:userData.id,
+  }).then((doc) => {
+    res.json(doc);
+  }).catch((err) => {
+    throw err;
+  });
+});
+
+
+app.get('/api/bookings', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const userData = await getUserDataFromReq(req);
+  res.json( await Booking.find({user:userData.id}).populate('place') );
+});
+
+app.listen(4000);
